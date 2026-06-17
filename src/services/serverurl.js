7f578b1046f51828401bd7a0ef282cdf72596ae6
Amalkdas <amalkdas095@gmail.com>
2026-06-17 18:53:18 +0530

    backend url fixed

--- src/services/serverurl.js
+++ src/services/serverurl.js
@@ -1,3 +1,3 @@
-export const serverurl = import.meta.env.BACKEND_URL
+export const serverurl = process.env.BACKEND_URL
 
 // export const serverurl = 'http://localhost:4000'
--- src/user/Pages/Chat.jsx
+++ src/user/Pages/Chat.jsx
@@ -6,7 +6,7 @@ import { useCallback } from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
 Link
 
-const url = import.meta.env.BACKEND_URL ;
+const url = process.env.BACKEND_URL ;
 // const url = 'http://localhost:4000'
 // this connects to the backend serv
 const socket = io(url);
